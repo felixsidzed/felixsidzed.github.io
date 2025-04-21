cee0213faeadbedf93b75f07ec502278be9c587c
TypicaIity <alboxqs.return@gmail.com>
2025-04-21 13:17:15 +0300

    Rewrite

---
+++ static/script.js
@@ -0,0 +1,3 @@
+function fuckassCat() {
+	window.open("https://media.discordapp.net/attachments/1314613383290425364/1363162535720648795/togif.gif?ex=680701ca&is=6805b04a&hm=807c1c17625dfd3ecd7ee580648f9d7a1f90815ccb42d69121d0bcf030dbe56e&=&width=1240&height=928")
+}
